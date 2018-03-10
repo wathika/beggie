c5e0bdcd351eca87a8f7c2bdedb616689eedde7e
wathika <vcwathika@gmail.com>
2018-03-10 05:09:20 +0300

    a project created returns js to the browser

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,4 +1,24 @@
 class ProjectController < ApplicationController
   def index
+    @project = Project.new
   end
+
+  def new
+    @project = Project.new(project_name)
+
+
+    respond_to do |format|
+      if @project.save
+        format.js
+      else
+        format.json {render json: @project.errors.messages, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  private
+    def project_name
+      params.require(:project).permit(:name, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'project/index'
+  get 'projects', to: 'project#index'
+
+  post 'projects', to: 'project#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
