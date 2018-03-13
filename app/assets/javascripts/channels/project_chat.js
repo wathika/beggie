824243a56ced96c27d9a9cdf1ba431117b76cfd6
wathika <vcwathika@gmail.com>
2018-03-13 11:32:05 +0300

    collecting form data and sending thru websockets

--- app/assets/javascripts/channels/project_chat.js
+++ app/assets/javascripts/channels/project_chat.js
@@ -1,5 +1,10 @@
 document.addEventListener('turbolinks:load', function(){
-  if(window.projectId) {
+
+  const messageForm = document.getElementById('new-message-form')
+  const messageInput = document.getElementById('message-input')
+  const messageContainer = document.getElementById('chat-contents')
+
+  if(window.projectId && messageForm) {
     App.project_chat = App.cable.subscriptions.create(
       {
         channel: "ProjectChatChannel",
@@ -16,7 +21,18 @@ document.addEventListener('turbolinks:load', function(){
       },
       received: function () {
 
+      },
+
+      sendMessage: function (content) {
+        this.perform('send_message', { content: content, project_id: window.projectId })
       }
+    }
+    )
+
+    messageForm.addEventListener('submit', function (e) {
+      e.preventDefault()
+      App.project_chat.sendMessage(messageInput.value)
+      messageInput.value = ''
     })
   }
 })
