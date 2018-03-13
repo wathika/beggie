73dec36d1b2b525070ca4e3316942898ac72d001
wathika <vcwathika@gmail.com>
2018-03-13 12:13:37 +0300

    tidying controller wich no longer handles any requests

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,17 +1,2 @@
 class MessageController < ApplicationController
-  def create
-    @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.js
-      end
-    end
-
-  end
-
-  protected
-    def message_params
-      params.require(:message).permit(:content, :project_id)
-    end
 end
